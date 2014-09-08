247f817fd7bd92d0a1d02ac05445314a93f8b933
Michael Bird <mike@b1rd.me>
2014-09-08 23:12:30 +0100

    Added unit tests. All pass against Chrome 37. Might want to ignore the 'multiple' tests due to performance reasons, but it's important the aggregate ones work.

---
+++ test/single.test.js
@@ -0,0 +1,192 @@
+'use strict';
+
+describe('single', function() {
+
+    beforeEach(function() {
+
+        sinon.spy(Object, 'observe');
+        sinon.spy(Object, 'unobserve');
+    });
+
+    afterEach(function() {
+
+        Object.observe.restore();
+        Object.unobserve.restore();
+    });
+
+    it('should call the handler when an object property is added', function(done) {
+
+        var subject = {},
+            handler = sinon.stub();
+
+        Object.observe(subject, handler);
+
+        subject.foobar = 123;
+
+        setTimeout(function() {
+            sinon.assert.calledOnce(handler);
+            sinon.assert.calledWithMatch(handler, numberOfChangesMatching(1));
+            sinon.assert.calledWithMatch(handler, aChangeMatching('add', 'foobar', 123));
+            done();
+        }, 100);
+    });
+
+    it('should not call the handler when an object property is added to the prototype', function(done) {
+
+        var subject = new Subject(),
+            handler = sinon.stub();
+
+        function Subject() {}
+
+        Object.observe(subject, handler);
+
+        Subject.prototype.foobar = 123;
+
+        setTimeout(function() {
+            assert.property(subject, 'foobar');
+            assert.propertyVal(subject, 'foobar', 123);
+            sinon.assert.notCalled(handler);
+            done();
+        }, 100);
+    });
+
+    it('should call the handler when an array item is added', function(done) {
+
+        var subject = [],
+            handler = sinon.stub();
+
+        Object.observe(subject, handler);
+
+        subject.push(123);
+
+        setTimeout(function() {
+            sinon.assert.calledOnce(handler);
+            sinon.assert.calledWithMatch(handler, numberOfChangesMatching(2));
+            sinon.assert.calledWithMatch(handler, aChangeMatching('add', '0', 123));
+            sinon.assert.calledWithMatch(handler, aChangeMatching('update', 'length', 1));
+            done();
+        }, 100);
+    });
+
+    it('should call the handler when an object property is updated', function(done) {
+
+        var subject = {},
+            handler = sinon.stub();
+
+        subject.foo = 'bar';
+
+        Object.observe(subject, handler);
+
+        subject.foo = 'barbar';
+
+        setTimeout(function() {
+            sinon.assert.calledOnce(handler);
+            sinon.assert.calledWithMatch(handler, numberOfChangesMatching(1));
+            sinon.assert.calledWithMatch(handler, aChangeMatching('update', 'foo', 'barbar', 'bar'));
+            done();
+        }, 100);
+    });
+
+    it('should not call the handler when an object property is updated on the prototype', function(done) {
+
+        var subject = new Subject(),
+            handler = sinon.stub();
+
+        function Subject() {}
+        Subject.prototype.foo = 'bar';
+
+        assert.property(subject, 'foo');
+        assert.propertyVal(subject, 'foo', 'bar');
+
+        Object.observe(subject, handler);
+
+        Subject.prototype.foo = 'barbar';
+
+        setTimeout(function() {
+            assert.property(subject, 'foo');
+            assert.propertyVal(subject, 'foo', 'barbar');
+            sinon.assert.notCalled(handler);
+            done();
+        }, 100);
+    });
+
+    it('should call the handler when an array item is updated', function(done) {
+
+        var subject = [],
+            handler = sinon.stub();
+
+        subject.push('bar');
+        
+        Object.observe(subject, handler);
+
+        subject[0] = 'barbar';
+
+        setTimeout(function() {
+            sinon.assert.calledOnce(handler);
+            sinon.assert.calledWithMatch(handler, numberOfChangesMatching(1));
+            sinon.assert.calledWithMatch(handler, aChangeMatching('update', '0', 'barbar', 'bar'));
+            done();
+        }, 100);
+    });
+
+    it('should call the handler when an object property is deleted', function(done) {
+
+        var subject = {},
+            handler = sinon.stub();
+
+        subject.foo = undefined;
+        
+        Object.observe(subject, handler);
+
+        delete subject.foo;
+
+        setTimeout(function() {
+            sinon.assert.calledOnce(handler);
+            sinon.assert.calledWithMatch(handler, numberOfChangesMatching(1));
+            sinon.assert.calledWithMatch(handler, aChangeMatching('delete', 'foo', undefined, undefined));
+            done();
+        }, 100);
+    });
+
+    it('should not call the handler when an object property is deleted on the prototype', function(done) {
+
+        var subject = new Subject(),
+            handler = sinon.stub();
+
+        function Subject() {}
+        Subject.prototype.foo = 123;
+
+        assert.property(subject, 'foo');
+        assert.propertyVal(subject, 'foo', 123);
+
+        Object.observe(subject, handler);
+
+        delete Subject.prototype.foo;
+
+        setTimeout(function() {
+            assert.notProperty(subject, 'foo');
+            sinon.assert.notCalled(handler);
+            done();
+        }, 100);
+    });
+
+    it('should call the handler when an array item is deleted', function(done) {
+
+        var subject = [],
+            handler = sinon.stub();
+
+        subject.push(undefined);
+
+        Object.observe(subject, handler);
+
+        subject.pop();
+
+        setTimeout(function() {
+            sinon.assert.calledOnce(handler);
+            sinon.assert.calledWithMatch(handler, numberOfChangesMatching(2));
+            sinon.assert.calledWithMatch(handler, aChangeMatching('delete', '0', undefined, undefined));
+            sinon.assert.calledWithMatch(handler, aChangeMatching('update', 'length', 0));
+            done();
+        }, 100);
+    });
+});
